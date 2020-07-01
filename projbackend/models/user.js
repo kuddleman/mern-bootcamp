e39872b5b4d9c9230472a706fd83511d579e50f9
Kuddleman <edl1155@gmail.com>
2020-07-01 11:34:41 -0700

    add authenticate method

--- projbackend/models/user.js
+++ projbackend/models/user.js
@@ -50,10 +50,15 @@ userSchema.virtual("password")
      this.salt = uuidv1
      this.encry_password = this.securePassword(password)
   })
-  .get()
+  .get(function(){
+      return this._password
+  })
 
 // create methods:
 userSchema.method = {
+  authenticate: function(plainpassword){
+    return this.securrePassword(plainpassword) === this.encry_password
+  },
   //get secure password: pass a plain password and return an encypted password
   securePassword: function(plainpassword) {
     if ( !password ) return ""
