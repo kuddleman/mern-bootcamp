d04ec2152ff528c7397cf391dcc43f342a4d257e
Kuddleman <edl1155@gmail.com>
2020-07-05 11:33:59 -0700

    complete connection between mongoose and database in app.js

--- projbackend/app.js
+++ projbackend/app.js
@@ -1,13 +1,23 @@
 const mongoose = require('mongoose')
+const chalk = require('chalk')
 const express = require('express')
 const app = express()
 
-mongoose.connect('mongodb://localhost:27017/myapp', 
-  {useNewUrlParser: true}
-)
+// mongoose.connect('mongodb://localhost:27017/myapp', 
+//   {useNewUrlParser: true}
+// )
+
+mongoose.connect('mongodb://localhost:27017/tshirt', { 
+  useNewUrlParser: true,
+  useUnifiedTopology: true,
+  useCreateIndex: true
+ }).then(() =>{
+   console.log(chalk.greenBright.inverse("DB CONNECTED"))
+ })
+
 
 const port = 8000
 
 app.listen(port, () => {
-  console.log(`App is running at ${ port }`)
+  console.log(chalk.magentaBright(`App is running at ${ port }`))
 })
