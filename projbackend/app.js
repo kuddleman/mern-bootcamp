ecf90df59e6e2143d6712349c14967ee0ccf6fa8
Kuddleman <edl1155@gmail.com>
2020-07-05 11:51:18 -0700

    add .env file

--- projbackend/app.js
+++ projbackend/app.js
@@ -1,22 +1,23 @@
+require('dotenv').config()
+
 const mongoose = require('mongoose')
 const chalk = require('chalk')
 const express = require('express')
 const app = express()
 
-// mongoose.connect('mongodb://localhost:27017/myapp', 
-//   {useNewUrlParser: true}
-// )
 
-mongoose.connect('mongodb://localhost:27017/tshirt', { 
+mongoose.connect( process.env.DATABASE, { 
   useNewUrlParser: true,
   useUnifiedTopology: true,
   useCreateIndex: true
  }).then(() =>{
    console.log(chalk.greenBright.inverse("DB CONNECTED"))
- })
+ }).catch(
+   console.log(chalk.red(`DB GOT OOOPS!`))
+ )
 
 
-const port = 8000
+const port = process.env.PORT || 8000
 
 app.listen(port, () => {
   console.log(chalk.magentaBright(`App is running at ${ port }`))
