01ce3b4782e7678db53a2543cc74ee4174cfd89b
Kuddleman <edl1155@gmail.com>
2020-07-25 16:10:29 -0700

    add validation to user input

--- projbackend/models/user.js
+++ projbackend/models/user.js
@@ -73,3 +73,4 @@ userSchema.methods = {
 }
 
 module.exports = mongoose.model('User', userSchema)
+
--- projbackend/routes/auth.js
+++ projbackend/routes/auth.js
@@ -3,9 +3,17 @@
 
 const express = require('express')
 const router = express.Router()
+const { check } = require('express-validator')
 const { signout, signup } = require('../controllers/auth')
 
-router.post("/signup", signup)
+router.post("/signup",[
+  check( 'name' )
+  .isLength({ min: 3 }).withMessage('must be at least 3 characters long'),
+  check( 'email' )
+  .isEmail(),
+  check( 'password' )
+  .isLength({ min: 5 }).withMessage('must be at least 5 characters long')
+], signup)
 
 router.get("/signout", signout ) 
 
