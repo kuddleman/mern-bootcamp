78e17c2b810f712087ba064dbebc92539f2afe1c
Kuddleman <edl1155@gmail.com>
2020-07-26 11:55:16 -0700

    add sign in route and token

--- projbackend/controllers/auth.js
+++ projbackend/controllers/auth.js
@@ -1,5 +1,7 @@
 const User = require('../models/user')
 const { check, validationResult } = require('express-validator')
+const jwt = require('jsonwebtoken')
+const expressJwt = require('express-jwt')
 
 exports.signup = ( req, res ) => {
   const errors = validationResult( req )
@@ -27,6 +29,41 @@ exports.signup = ( req, res ) => {
     })
   })
 }
+
+exports.signin = ( req, res ) => {
+  const { email, password } = req.body
+
+  if( !errors.isEmpty()) {
+    return res.status( 422 ).json({
+      error: errors.array()[0].msg
+    })
+  }
+
+  User.findOne({ email }, ( err, user ) => {
+    if ( err ) {
+      res.status( 400 ).json({
+        error: "USER email does not exist"
+      })
+    }
+
+    if ( !user.authenticate( password )) {
+       return res.status( 401 ).json({
+         error: "Email and password do not match"
+      })
+    }
+    // create token:
+    const token = jwt.sign({ _id: user._id }, process.env.SECRET)
+
+    // put token in cookie:
+    res.cookie( "token", token, { expire: new Date() + 9999 } )
+
+    // send response to front end:
+    const { _id, name, email, role } = user
+    return res.json({ token, user: { _id, name, email, role } })
+  })
+}
+
+
 
 
 exports.signout = ( req, res ) => {
--- projbackend/routes/auth.js
+++ projbackend/routes/auth.js
@@ -4,16 +4,30 @@
 const express = require('express')
 const router = express.Router()
 const { check } = require('express-validator')
-const { signout, signup } = require('../controllers/auth')
+const { signout, signup, signin } = require('../controllers/auth')
 
-router.post("/signup",[
-  check( 'name' )
-  .isLength({ min: 3 }).withMessage('name must be at least 3 letters long'),
-  check( 'email' )
-  .isEmail().withMessage('email must be formatted correctly'),
-  check( 'password' )
-  .isLength({ min: 5 }).withMessage('password must be at least 5 characters long')
-], signup)
+router.post(
+  "/signup",
+  [
+    check( 'name' )
+    .isLength({ min: 3 }).withMessage('name must be at least 3 letters long'),
+    check( 'email' )
+    .isEmail().withMessage('email must be formatted correctly'),
+    check( 'password' )
+    .isLength({ min: 5 }).withMessage('password must be at least 5 characters long')
+  ], 
+  signup
+)
+router.post(
+  "/signin",
+  [
+    check( 'email' )
+    .isEmail().withMessage('email must be formatted correctly'),
+    check( 'password' )
+    .isLength({ min: 1 }).withMessage('password field is required')
+  ], 
+  signin
+)
 
 router.get("/signout", signout ) 
 
