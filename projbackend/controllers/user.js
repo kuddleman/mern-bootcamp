b37612be1f65ecaf49c9ecf80892807a136d9502
Kuddleman <edl1155@gmail.com>
2020-07-27 14:06:42 -0700

    add put request

--- projbackend/controllers/user.js
+++ projbackend/controllers/user.js
@@ -21,6 +21,31 @@ exports.getUser = ( req, res ) => {
   return res.json( req.profile )
 }
 
+exports.updateUser = ( req, res ) => {
+  User.findByIdAndUpdate(
+    { _id: req.profile._id },
+    { $set: req.body },
+    { new: true, useFindAndModify: false },
+    ( err, user ) => {
+      if ( err ) {
+        return res.status( 400 ).json({
+          error: "You are not authorized to update this user"
+        })
+      }
+      user.salt = undefined
+      user.encry_password = undefined
+      user.createdAt = undefined
+      user.updatedAt = undefined
+      res.json( user )
+    }
+  )
+}
+
+
+
+
+// getAllUsers  method was just an exercise:
+
 // exports.getAllUsers = ( req, res ) => {
 //   User.find().exec( (err, users)  => {
 //     if ( err || !users ) {
