f7e679b1c4cf3928aaee1c95f5e004fa4cc9de04
Kuddleman <edl1155@gmail.com>
2020-07-27 11:54:39 -0700

    complete user routes

--- projbackend/routes/user.js
+++ projbackend/routes/user.js
@@ -1,10 +1,12 @@
 const express = require('express')
 const router = express.Router()
 
-const { getUserById } = require('../controllers/user')
-const {} = require('../controllers/auth')
+const { getUserById, getUser } = require('../controllers/user')
+const { isSignedIn, isAuthenticated, isAdmin  } = require('../controllers/auth')
 
-router.param('userID', getUserById)
+router.param('userId', getUserById)
+
+router.get('/user/:userId', isSignedIn, isAuthenticated, getUser)
 
 module.exports = router
 
