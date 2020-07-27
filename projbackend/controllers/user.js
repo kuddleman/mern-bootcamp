1b76ca2667c5b3cb6cede693cd48e7fb32cf1e1b
Kuddleman <edl1155@gmail.com>
2020-07-27 14:47:13 -0700

    add pushOrderInPurchaseList method

--- projbackend/controllers/user.js
+++ projbackend/controllers/user.js
@@ -55,6 +55,39 @@ exports.userPurchaseList = ( req, res ) => {
   })
 }
 
+// this is middleware
+exports.pushOrderInPurchaseList = ( req, res, next ) => {
+
+  let purchases = []
+  req.body.order.produts.forEach( product => {
+    purchases.push({
+      _id: product._id,
+      name: product.name,
+      description: product.description,
+      category: product.category,
+      quantity: product.quantity,
+      amount: req.body.order.amount,
+      transaction_id: req.body.order.transaction_id
+
+    })
+  })
+
+  // store this is DB
+  User.findOneAndUpdate(
+    { _id: req.profile._id },
+    { $push: { purchases: purchases }},
+    { new: true },
+    ( err, purchases ) => {
+       if ( err ) {
+         return res.status( 400 ).json({
+           error: 'Unable to save purchase list'
+         })
+       }
+       next()
+    }
+  )
+}
+
 
 
 
