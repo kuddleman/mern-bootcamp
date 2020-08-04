c7295c3fd12839a1d992a764c1fcf3c85509d31d
Kuddleman <edl1155@gmail.com>
2020-08-04 14:47:07 -0700

    set up product controllers and routes

---
+++ projbackend/routes/product.js
@@ -0,0 +1,14 @@
+const express = require('express')
+const router = express.Router()
+
+const { getProductById } = require('../controllers/product')
+const { isSignedIn, isAuthenticated, isAdmin } = require('../controllers/auth')
+const { getUserById } = require('../controllers/user')
+
+module.exports = router
+
+//params:
+router.param('userId', getUserById)
+router.param('productId', getProductById)
+
+//routes:
